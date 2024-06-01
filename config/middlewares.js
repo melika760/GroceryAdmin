6066a9eba8faecfdc3541b4de4bad3c579ab2efa
melika760 <melikaaatafazoli@gmail.com>
2024-06-01 13:20:17 +0100

    add cloudinary

--- config/middlewares.js
+++ config/middlewares.js
@@ -8,26 +8,25 @@ module.exports = [
         'strapi::session',
         'strapi::favicon',
         'strapi::public',
-        'strapi::security',
-    // {
-    //   name: 'strapi::security',
-    //   config: {
-    //     contentSecurityPolicy: {
-    //       useDefaults: true,
-    //       directives: {
-    //         'connect-src': ["'self'", 'https:'],
-    //         'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
-    //         'media-src': [
-    //           "'self'",
-    //           'data:',
-    //           'blob:',
-    //           'market-assets.strapi.io',
-    //           'res.cloudinary.com',
-    //         ],
-    //         upgradeInsecureRequests: null,
-    //       },
-    //     },
-    //   },
-    // },
+    {
+      name: 'strapi::security',
+      config: {
+        contentSecurityPolicy: {
+          useDefaults: true,
+          directives: {
+            'connect-src': ["'self'", 'https:'],
+            'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+            'media-src': [
+              "'self'",
+              'data:',
+              'blob:',
+              'market-assets.strapi.io',
+              'res.cloudinary.com',
+            ],
+            upgradeInsecureRequests: null,
+          },
+        },
+      },
+    },
     // ...
   ];
